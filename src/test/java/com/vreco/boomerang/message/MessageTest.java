e9e2144e53128cafda8b3090b4080dc2624a83e0
Ben Aldrich <ben.aldrich@returnpath.net>
2012-08-11 16:10:06 -0600

    Add message tests

--- src/test/java/com/vreco/boomerang/message/MessageTest.java
+++ src/test/java/com/vreco/boomerang/message/MessageTest.java
@@ -3,6 +3,7 @@ package com.vreco.boomerang.message;
 import com.vreco.boomerang.conf.Conf;
 import com.vreco.boomerang.conf.MockConf;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -33,7 +34,7 @@ public class MessageTest extends TestCase {
 
   public void testCreateExpectedMsg() throws IOException {
     ObjectMapper mapper = new ObjectMapper();
-    String json = "{\"test\":\"proc\",\"something\":\"uuid\", \"" + conf.getValue("boomerang.producer.label") 
+    String json = "{\"test\":\"proc\",\"something\":\"uuid\", \"" + conf.getValue("boomerang.producer.label")
             + "\":\"test\"}";
     HashMap<String, Object> hMsg = mapper.readValue(json, HashMap.class);
     Message msg = new Message(hMsg, conf);
@@ -41,4 +42,40 @@ public class MessageTest extends TestCase {
     Assert.assertNotNull(msg.getUuid());
   }
 
+  /**
+   * Test to see if date already exist in the message we should use it.
+   */
+  public void testMessageWithExistingDate() throws Exception {
+
+    String expDate = "2012081115584545";
+
+    ObjectMapper mapper = new ObjectMapper();
+    String json = "{\"test\":\"proc\",\"something\":\"uuid\", \"" + conf.getValue("boomerang.producer.label")
+            + "\":\"test\", \"" + conf.getValue("boomerang.date.label") + "\":\"" + expDate + "\"}";
+    HashMap<String, Object> hMsg = mapper.readValue(json, HashMap.class);
+    Message msg = new Message(hMsg, conf);
+    Assert.assertEquals("test", msg.getDestination());
+    Assert.assertNotNull(msg.getUuid());
+    Date exp = msg.msgDateFormat.parse(expDate);
+    Assert.assertEquals(exp, msg.getDate());
+
+  }
+
+  /**
+   * Test to see if date already exist in the message we should use it.
+   */
+  public void testMessageWithExistingUUID() throws Exception {
+
+    String exp= "abc-2352627-6%^";
+
+    ObjectMapper mapper = new ObjectMapper();
+    String json = "{\"test\":\"proc\",\"something\":\"uuid\", \"" + conf.getValue("boomerang.producer.label")
+            + "\":\"test\", \"" + conf.getValue("boomerang.uuid.label") + "\":\"" + exp + "\"}";
+    HashMap<String, Object> hMsg = mapper.readValue(json, HashMap.class);
+    Message msg = new Message(hMsg, conf);
+    Assert.assertEquals("test", msg.getDestination());
+    Assert.assertNotNull(msg.getUuid());
+    Assert.assertEquals(exp, msg.getUuid());
+
+  }
 }
